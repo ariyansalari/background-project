be7200ad2233ab4a207fae907410205dba13b224
ariyan Salari <ariyan4509@gmail.com>
2023-02-06 12:12:07 -0800

    background project

---
+++ index.js
@@ -0,0 +1,11 @@
+const Submit=document.querySelector(".submit-form");
+const ListElement=document.querySelector("#ListElement");
+const Username=document.querySelector("#Username");
+const UsernameList=document.querySelector("#Username-form")
+const color=document.querySelector("#save-color")
+
+Submit.addEventListener("click",(e)=>{
+ListElement.style.display="flex"
+ListElement.style.top="25rem"
+
+})
