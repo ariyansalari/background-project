5770a7d70399cb83f1df97be59f842ca64c8898d
ariyan Salari <ariyan4509@gmail.com>
2023-02-08 08:35:43 -0800

    complete back ground project

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const Submit=document.querySelector(".submit-form");
 const ListElement=document.querySelector("#ListElement");
-const Username=document.querySelector("#Username");
+const Username=document.querySelector("#username");
 const UsernameList=document.querySelector("#Username-form")
-const color=document.querySelector("#save-color")
+const Primarycolor=document.querySelector("#primary-color")
+const Secondarycolor=document.querySelector("#secondary-color")
+const profileColor=document.querySelector("#profile-color")
+const inputs=document.querySelectorAll(".image");
+const SaveSubmit=document.querySelector("#submit-save");
+const Avatar=document.querySelector("#Avatar")
+const backgroundColor=document.querySelector(".background")
+let selectedAvatar;
+inputs.forEach((i)=>{
+
+       i.addEventListener("click",(e)=>{
+        
+        selectedAvatar=e.target.previousElementSibling.src;
+       })
+
+    
+    
+
+
+})
+SaveSubmit.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+ListElement.style.top="-80rem";
+UsernameList.innerHTML=Username.value.length>0?Username.value:UsernameList.textContent
+   
+console.log(UsernameList)
+    Username.value="";
+    Avatar.src=selectedAvatar == null ? Avatar.src : selectedAvatar;
+    
+  
+    document.querySelector(":root").style.setProperty("--primary",Primarycolor.value);
+document.querySelector(":root").style.setProperty("--secondary",Secondarycolor.value);
+document.querySelector(":root").style.setProperty("--thirth",profileColor.value);
+})
+window.addEventListener("click",(e)=>{
+    if(e.target==ListElement)
+    {
+        ListElement.style.display="none";
+    }
+})
+
 
 Submit.addEventListener("click",(e)=>{
-ListElement.style.display="flex"
-ListElement.style.top="25rem"
+    e.preventDefault();
+ListElement.style.top="0.01rem"
 
 })
